70c947c5181f7e58be7179584adef0504b9b4396
Weipeng He <weipeng.he@idiap.ch>
2018-05-10 20:36:07 +0200

    add easy deploy code

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+BRANCH=master
+GITIO_REPO=git@github.com:hwp/hwp.github.io.git
+GITIO_REPO_PATH=gitio_repo
+PELICAN_OUT_PATH=output
+
+# get head reference number
+headref=$(git show-ref HEAD)
+
+# clone io repo
+git clone --branch=$BRANCH $GITIO_REPO $GITIO_REPO_PATH || echo 'github.io repo exist' &&
+
+# copy data we're interested in to that directory
+rsync -av --delete --exclude=.git $PELICAN_OUT_PATH/ $GITIO_REPO_PATH/ &&
+
+# go into
+cd $GITIO_REPO_PATH &&
+
+# add, commit and push files
+git add -f . &&
+git commit -m "Deploy '$headref' to Github Pages" &&
+git push -f origin $BRANCH > /dev/null &&
+
+echo 'Deploy completed' || echo 'Deploy failed'
+
